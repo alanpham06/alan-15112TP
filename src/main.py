d87aa84109b1da18d92bac2cac82f3d5dee9f34a
= <=>
2024-11-18 14:16:26 -0500

    Initialized CMU graphics code

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print('hello world')
+from cmu_graphics import *
+
+def onAppStart(app):
+    pass
+
+def redrawAll(app):
+    pass
+
+def onMousePress(app):
+    pass
+
+def onMouseRelease(app):
+    pass
+
+def onStep(app):
+    pass
+
+def main():
+    runApp()
+
+main()
